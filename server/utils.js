10f1359a99dd62e2f7b6e84f5921eed700eb666f
DarrylAdigwu <115912161+DarrylAdigwu@users.noreply.github.com>
2025-03-29 19:03:57 -0700

    added function for tokens and modified others

--- server/utils.js
+++ server/utils.js
@@ -1,9 +1,9 @@
 import * as argon2 from "argon2";
 import jwt from "jsonwebtoken";
 
-export async function checkString(str) {
-  const regex = /\W/;
-  const match = await str.match(regex);
+export function checkString(str) {
+  const regex = /^[a-zA-Z0-9\s]*$/;
+  const match = str.match(regex);
   return match;
 }
 
@@ -22,3 +22,18 @@ export async function JWT(payload, privateKey) {
   const token = jwt.sign(payload, privateKey, {expiresIn: `${1000 * 60 * 60}`})
   return token;
 }
+
+// Format date 
+export function formatDate(date) {
+  const passedInDate = Date.parse(date)
+  const plannedDate = new Date(passedInDate)
+  
+  const year = plannedDate.getFullYear();
+  const month = String(plannedDate.getMonth() + 1).padStart(2, '0');
+  const day = String(plannedDate.getDate()).padStart(2, '0');
+  const newFormat = `${year}-${month}-${day}`;
+  
+  return newFormat;
+}
+
+
